2560a447f8d0b26094ad19c11b2bce6549faa6fe
syfnazkria <www.fynazakaria@gmail.com>
2025-01-18 05:32:42 +0800

    sec

--- src/server/Handlers/HomeHandler.java
+++ src/server/Handlers/HomeHandler.java
@@ -10,9 +10,10 @@ import java.nio.file.Paths;
 public class HomeHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        // Read the updated index.html template from the file system
         String response = new String(Files.readAllBytes(Paths.get("src/templates/index.html")));
-        exchange.sendResponseHeaders(200, response.getBytes().length);
-        exchange.getResponseBody().write(response.getBytes());
-        exchange.close();
+        exchange.sendResponseHeaders(200, response.getBytes().length); // Send response with content length
+        exchange.getResponseBody().write(response.getBytes()); // Write content to response body
+        exchange.close(); // Close the exchange
     }
 }
