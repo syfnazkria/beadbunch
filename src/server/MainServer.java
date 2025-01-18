ee8ab54c41efcac72e45dc877533e224a8f5128f
Athrh2 <athirah.ibrahim04@gmail.com>
2025-01-18 09:32:28 +0800

    Athi1 commit

--- src/server/MainServer.java
+++ src/server/MainServer.java
@@ -1,10 +1,15 @@
 package server;
 
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
 import server.Handlers.*;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class MainServer {
     public static void main(String[] args) throws IOException {
@@ -17,8 +22,28 @@ public class MainServer {
         server.createContext("/cart", new CartHandler());
         server.createContext("/checkout", new CheckoutHandler());
 
+        // Add static file serving for /resources
+        server.createContext("/resources", new StaticFileHandler());
+
         server.setExecutor(null); // Default executor
         System.out.println("Server is running on http://localhost:8080");
         server.start();
+    }
+
+    static class StaticFileHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String filePath = "src" + exchange.getRequestURI().getPath();
+            try {
+                byte[] fileBytes = Files.readAllBytes(Paths.get(filePath));
+                exchange.sendResponseHeaders(200, fileBytes.length);
+                OutputStream os = exchange.getResponseBody();
+                os.write(fileBytes);
+                os.close();
+            } catch (IOException e) {
+                exchange.sendResponseHeaders(404, 0);
+                exchange.getResponseBody().close();
+            }
+        }
     }
 }
